7ef53a5610ccbf898873977659251e83fbabe5d6
TbailaH <hamza.tbaila@uit.ac.ma>
2025-04-23 16:20:01 +0100

    a

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@ $prefix = isset($admin) ? '../' : '';
   <title>Agence de Voyages</title>
   <link rel="stylesheet" href="<?php echo $prefix; ?>css/style.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  
 </head>
 <body>
 
